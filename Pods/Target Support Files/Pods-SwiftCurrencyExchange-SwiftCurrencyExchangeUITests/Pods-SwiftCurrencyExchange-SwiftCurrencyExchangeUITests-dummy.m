5e7144c605617d5ca2dfb83e80a8858019f990a4
Faisal Tanveer <faisal.tanveer@innotech-sa.com>
2023-08-23 13:47:55 +0500

    updated code

--- Pods/Target Support Files/Pods-SwiftCurrencyExchange-SwiftCurrencyExchangeUITests/Pods-SwiftCurrencyExchange-SwiftCurrencyExchangeUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftCurrencyExchange_SwiftCurrencyExchangeUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftCurrencyExchange_SwiftCurrencyExchangeUITests
-@end
